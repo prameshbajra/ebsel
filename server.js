f8d4bb7307687bf54cc9471e8ac2b7358ecd2db4
prameshbajra <pe.messh@gmail.com>
2019-02-09 09:32:43 +0530

    Adding to heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + "/dist/ebsel"));
 
 app.get("/*", (req, res, next) => {
-    res.sendFile(path.join(__dirname + "/dist/ebsel/index.html"));
+    res.sendFile(path.join(__dirname + "dist/ebsel/index.html"));
 })
 
 
